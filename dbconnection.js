f112250d89e5dbc27bb66565b0ed56906846862f
ycas2002 <ycas2002@yahoo.com>
2019-05-04 14:26:29 +0200

    reinitial commit

---
+++ dbconnection.js
@@ -0,0 +1,15 @@
+//var mysql = require('mysql');
+//var connection = mysql.createPool({
+//    host: 'localhost',
+//    user: 'root',
+//    password: '',
+//    database: 'resttest'
+//});
+//
+//module.exports = connection;
+
+var JsonDB = require('node-json-db');
+
+// var connection = new JsonDB("car_purchases", true, false);
+
+module.exports = connection;
---
+++ routes/listing.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+  const rp = require('request-promise');
+  const requestOptions = {
+    method: 'GET',
+    uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    qs: {
+      'start': '1',
+      'limit': '5000',
+      'convert': 'USD'
+    },
+    headers: {
+      'X-CMC_PRO_API_KEY': '2e8a7af8-4565-4b89-ae14-4086449c9da8'
+    },
+    json: true,
+    gzip: true
+  };
+  rp(requestOptions).then(response => {
+    res.send(response);
+  }).catch((err) => {
+    console.log('API call error:', err.message);
+  });
+});
+
+module.exports = router;
